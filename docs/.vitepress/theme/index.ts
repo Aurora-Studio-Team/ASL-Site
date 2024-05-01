38a4e6345fa6655d4f7707044c3792c8329106d2
THZ <thz15916108729@163.com>
2024-05-01 13:22:53 +0800

    v1.1.4

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,8 @@ import Theme from 'vitepress/theme'
 import './style/var.css'
 import './style/link.css'
 import './style/linkcard.css'
+/* .vitepress\theme\index.ts */
 
 export default {
-  ...Theme
+  ...Theme,
 }
